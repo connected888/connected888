51ef65b142b35a15f6e3b09ba20a6fdd264ebae8
connected888 <billing@connected888.org>
2025-09-12 19:29:24 -0500

    added promote page

--- src/app/components/promote/promote.ts
+++ src/app/components/promote/promote.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  ReactiveFormsModule,
+  Validators,
+  FormGroup,
+  FormControl,
+  NonNullableFormBuilder
+} from '@angular/forms';
+
+type PromoteForm = {
+  orgName: FormControl<string>;
+  contactName: FormControl<string>;
+  contactEmail: FormControl<string>;
+};
 
 @Component({
   selector: 'app-promote',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './promote.html',
-  styleUrl: './promote.scss'
+  styleUrls: ['./promote.scss']
 })
-export class Promote {
+export class Promote implements OnInit {
+  form!: FormGroup<PromoteForm>;
 
+  constructor(private fb: NonNullableFormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      orgName: this.fb.control('', { validators: [Validators.required, Validators.minLength(2)] }),
+      contactName: this.fb.control('', { validators: [Validators.required, Validators.pattern(/^[A-Za-z][A-Za-z .'-]{1,}$/)] }),
+      contactEmail: this.fb.control('', { validators: [Validators.required, Validators.email] })
+    });
+  }
+
+  get f(): PromoteForm { return this.form.controls; }
+
+  submit(): void {
+    this.form.markAllAsTouched();
+    if (this.form.invalid) return;
+    console.log('Promote payload', this.form.getRawValue());
+    alert('Submitted!');
+  }
 }
